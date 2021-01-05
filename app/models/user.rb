eba4f9d0ec1f2e84322ff84b0cdd58c1b43ffe44
jennifer tracy <jenniferetracy@gmail.com>
2021-01-05 09:59:45 -0500

    update to random_restaurant

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,14 +9,19 @@ has_many :restaurants, through: :selections
         cuisine_choice = Cuisine.all.find{|cuisine| cuisine.id == cuisine_id}
         cuisine_choice.restaurants.sample
     end
-    
-    def create_selection(cuisine_id)
+
+    def create_selection_by_cuisine(cuisine_id)
         restaurant_id_inst = random_by_cuisine(cuisine_id).id
         Selection.create(restaurant_id: restaurant_id_inst, user_id: self.id)
     end  
  
 
+    # def random_restaurant
+    #     Restaurant.all.sample
+    # end  
+
     def random_restaurant
-        Restaurant.all.sample
-    end  
+        restaurant_id_inst = Restaurant.all.sample.id
+        Selection.create(restaurant_id: restaurant_id_inst, user_id: self.id)
+    end 
 end
