8aaf031ff9e7f776cf498d005135e3759cac0bbd
jennifer tracy <jenniferetracy@gmail.com>
2021-01-07 17:50:14 -0500

    remove api from DateNight.rb

--- app/DateNight.rb
+++ app/DateNight.rb
@@ -16,20 +16,6 @@ class DateNight
     # get_joke(what_subject)
   end
 
-  def api_restaurants
-    response =  RestClient.get "https://developers.zomato.com/api/v2.1/search?count=200&lat=40.7128&lon=74.0060&radius=40233.6&sort=real_distance", {content_type: :json, accept: :json, "user-key": "b15c0449cabc39ad8fcc569d26d3c0a0"}
-
-    body = response.body
-    parsed = JSON.parse(body)
-    name = parsed["restaurants"].map{|restaurant| restaurant["restaurant"]["name"]}
-    url = parsed["restaurants"].map{|restaurant| restaurant["restaurant"]["url"]}
-    street_address = parsed["restaurants"].map{|restaurant| restaurant["restaurant"]["location"]["address"]}
-    cuisine = parsed["restaurants"].map{|restaurant| restaurant["restaurant"]["cuisines"]}
-    hours = parsed["restaurants"].map{|restaurant| restaurant["restaurant"]["timimgs"]}
-    phone_number = parsed["restaurants"].map{|restaurant| restaurant["restaurant"]["phone_numbers"]}
-
-
-  end
 
   def welcome
     puts "Welcome to Date Night!"
